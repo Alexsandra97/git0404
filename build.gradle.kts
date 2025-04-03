3c0fe73352a87a1ae568d54c415b779a68caa568
Alexsandra97 <thefirstone010101@gmail.com>
2025-04-03 23:55:09 +1000

    2com

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,30 @@
-plugins {
-    kotlin("jvm") version "2.1.10"
-}
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+plugins {
+    kotlin("jvm") version "1.9.22"
+    application
+}
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.github.kotlin-telegram-bot:kotlin-telegram-bot:6.3.0")
 }
 
-tasks.test {
-    useJUnitPlatform()
+// Фиксируем одну версию JVM для всех задач компиляции
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "21"  // Меняем на 21 (как у Java)
 }
-kotlin {
-    jvmToolchain(21)
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "21"
+    targetCompatibility = "21"
+}
+
+application {
+    mainClass.set("MainKt")
 }
--- src/main/kotlin/bt.kt
+++ src/main/kotlin/bt.kt
@@ -1,3 +1,22 @@
-fun main(){
-    println("Hello METANIT.COM")
+import com.github.kotlintelegrambot.bot
+import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.text
+import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.ParseMode.MARKDOWN
+
+fun main() {
+    val bot = bot {
+        token = "7674778480:AAGs3VAMpMTb4A82IDXgYPwPu4uiI1s8DLw"  // ← Замените на свой!
+        dispatch {
+            command("start") {
+                bot.sendMessage(
+                    chatId = ChatId.fromId(message.chat.id),
+                    text = "Привет! Я бот на Kotlin 🤖"
+                )
+            }
+        }
+    }
+    bot.startPolling()
+    println("Бот запущен! Остановить: Ctrl+C")
 }
